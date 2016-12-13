c0c74d2f1b5f17daf2077d2254205ead417031bb
Jérémy K <jeremy@keusters.be>
2016-12-13 15:46:37 +0100

    Added the QR-Code scanner

--- gingerwaldApp/www/js/app.js
+++ gingerwaldApp/www/js/app.js
@@ -27,8 +27,7 @@ angular.module('gingerwald', ['ionic', 'gingerwald.controllers'])
 
   .state('login', {
     url: '/login',
-    templateUrl: 'templates/login.html',
-    controller: 'LoginCtrl'
+    templateUrl: 'templates/login.html'
   })
 
     .state('app', {
@@ -57,7 +56,8 @@ angular.module('gingerwald', ['ionic', 'gingerwald.controllers'])
       url: '/scan-a-l',
       views: {
         'menuContent': {
-          templateUrl: 'templates/scan-a-l.html'
+          templateUrl: 'templates/scan-a-l.html',
+          controller: 'QrCodeScanner'
         }
       }
     })
--- gingerwaldApp/www/js/controllers.js
+++ gingerwaldApp/www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('gingerwald.controllers', [])
+angular.module('gingerwald.controllers', ['ionic', 'ngCordova'])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
@@ -21,3 +21,17 @@ angular.module('gingerwald.controllers', [])
     $scope.key = data.Login;
   })
 })
+
+.controller("QrCodeScanner", function($scope, $cordovaBarcodeScanner) {
+ 
+    $scope.scanBarcode = function() {
+        $cordovaBarcodeScanner.scan().then(function(imageData) {
+            alert(imageData.text);
+            console.log("Barcode Format -> " + imageData.format);
+            console.log("Cancelled -> " + imageData.cancelled);
+        }, function(error) {
+            console.log("An error happened -> " + error);
+        });
+    };
+ 
+});
