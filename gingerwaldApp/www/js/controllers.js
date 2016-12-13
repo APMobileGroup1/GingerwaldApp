c34f33e35cc1a3532525483a4045621a448cece5
Jérémy K <jeremy@keusters.be>
2016-12-13 14:11:53 +0100

    Cleaned up the controller.js

--- gingerwaldApp/www/js/controllers.js
+++ gingerwaldApp/www/js/controllers.js
@@ -1,7 +1,5 @@
 angular.module('gingerwald.controllers', [])
 
-
-
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
   // With the new view caching in Ionic, Controllers are only called
@@ -11,91 +9,15 @@ angular.module('gingerwald.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-
   $scope.info = function(){
 
     swal("Oeps!", "Hier is nog niets te vinden. We zijn hard aan het werken op deze functie. Sit tight!", "warning");
-
   }
-})
-
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+  
 })
 
 .controller('MainCtrl', function($scope, $http, $rootScope){
-  $rootScope.token="RDN8suCd9Unll6zThEiXvUViJiyrGH3bqa3gE7pQdSti1S7nwk6ekzA4MrGawBmu"
-  $http.jsonp('https://www.gingerwald.com/community/v2.1/api/getUserDetails.php?token=' + $rootScope.token + '&callback=JSON_CALLBACK').success(function(data){
+  $rootScope.token = "RDN8suCd9Unll6zThEiXvUViJiyrGH3bqa3gE7pQdSti1S7nwk6ekzA4MrGawBmu"; $http.jsonp('https://www.gingerwald.com/community/v2.1/api/getUserDetails.php?token=' + $rootScope.token + '&callback=JSON_CALLBACK').success(function(data){
     $scope.key = data.Login;
   })
 })
-
-
-.controller('LoginCtrl', function($scope, $stateParams) {
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-  };
-});
-
-/*.controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state) {
-
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-  };
-
-
-  $scope.data = {};
-
-  $scope.login = function() {
-    LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
-      $state.go('tab.dash');
-    }).error(function(data) {
-      var alertPopup = $ionicPopup.alert({
-        title: 'Login failed!',
-        template: 'Please check your credentials!'
-      });
-    });
-  }
-})
-
-  */
