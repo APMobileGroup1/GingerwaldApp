5f353dab286eb552e3e7a2137e830b64cbb6c998
Jérémy K <jeremy@keusters.be>
2016-12-17 11:49:34 +0100

    Added some more scan functionality

--- gingerwaldApp/www/js/controllers.js
+++ gingerwaldApp/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('gingerwald.controllers', ['ionic', 'ngCordova'])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -9,38 +9,41 @@ angular.module('gingerwald.controllers', ['ionic', 'ngCordova'])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.info = function(){
+  $scope.info = function () {
 
     swal("Oeps!", "Hier is nog niets te vinden. We zijn hard aan het werken op deze functie. Sit tight!", "warning");
   }
-  
+
 })
 
-.controller('MainCtrl', function($scope, $http, $rootScope){
-  $rootScope.token = "RDN8suCd9Unll6zThEiXvUViJiyrGH3bqa3gE7pQdSti1S7nwk6ekzA4MrGawBmu"; $http.jsonp('https://www.gingerwald.com/community/v2.1/api/getUserDetails.php?token=' + $rootScope.token + '&callback=JSON_CALLBACK').success(function(data){
+.controller('MainCtrl', function ($scope, $http, $rootScope) {
+  $rootScope.token = "RDN8suCd9Unll6zThEiXvUViJiyrGH3bqa3gE7pQdSti1S7nwk6ekzA4MrGawBmu";
+  $http.jsonp('https://www.gingerwald.com/community/v2.1/api/getUserDetails.php?token=' + $rootScope.token + '&callback=JSON_CALLBACK').success(function (data) {
     $scope.key = data.Login;
   })
 })
 
-.controller("QrCodeScanner", function($scope, $cordovaBarcodeScanner) {
-    
-    var testURL = "http://qr.gingerwald.com?b=ee6sDR2K26xHUE";    
-    var regex = /[^=]*$/;
-  
-    $scope.scanned2 = regex.exec(testURL)[0];
-  
-    $scope.scanBarcode = function() {
-        $cordovaBarcodeScanner.scan().then(function(imageData) {
-            
-            $scope.scanned = imageData.text;
-            console.log("Barcode Format -> " + imageData.format);
-            console.log("Cancelled -> " + imageData.cancelled);
-        }, function(error) {
-            console.log("An error happened -> " + error);
-        });
-    };
-  
-    
-  
- 
+.controller("QrCodeScanner", function ($scope, $cordovaBarcodeScanner) {
+
+  var testURL = "http://qr.gingerwald.com?b=ee6sDR2K26xHUE";
+  var regex = /[^=]*$/;
+
+  $scope.scanned2 = regex.exec(testURL)[0];
+  $scope.scanned2 += "3";
+
+  $scope.scanBarcode = function () {
+    $cordovaBarcodeScanner.scan({
+      "showFlipCameraButton": true,
+      "showTorchButton": true
+    }).then(function (imageData) {
+
+      $scope.scanned = imageData.text;
+      console.log("Barcode Format -> " + imageData.format);
+      console.log("Cancelled -> " + imageData.cancelled);
+    }, function (error) {
+      console.log("An error happened -> " + error);
+    });
+  };
+
+
 });
