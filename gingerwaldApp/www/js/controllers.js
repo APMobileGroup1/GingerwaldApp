287905aa859352e36968ce0d3b9add37ce28c441
Compulsor <schelfhoutlennart@gmail.com>
2016-12-18 14:52:06 +0100

    Added angular-chart.js
    Chart added to the dashboard menu. Just as an example, still needs
    tweaking.

--- gingerwaldApp/www/js/controllers.js
+++ gingerwaldApp/www/js/controllers.js
@@ -1,7 +1,5 @@
 angular.module('gingerwald.controllers', [])
 
-
-
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
   // With the new view caching in Ionic, Controllers are only called
@@ -11,99 +9,35 @@ angular.module('gingerwald.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-
   $scope.info = function(){
 
     swal("Oeps!", "Hier is nog niets te vinden. We zijn hard aan het werken op deze functie. Sit tight!", "warning");
-
   }
-})
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
 })
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-})
-
-/**.controller('MainCtrl', function($scope, $http, $rootScope){
-  $rootScope.token="RDN8suCd9Unll6zThEiXvUViJiyrGH3bqa3gE7pQdSti1S7nwk6ekzA4MrGawBmu"
-  $http.jsonp('https://www.gingerwald.com/community/v2.1/api/getUserDetails.php?token=' + $rootScope.token + '&callback=JSON_CALLBACK').success(function(data){
-    $scope.key = data.Login;
-  })
-})*/
 
 .controller('MainCtrl', function($scope, $http, $rootScope){
-
-  $http.get('https://www.gingerwald.com/community/v2.1/api/getUserDetails.php?token=RDN8suCd9Unll6zThEiXvUViJiyrGH3bqa3gE7pQdSti1S7nwk6ekzA4MrGawBmu').success(function(data){
+    $http.get('https://www.gingerwald.com/community/v2.1/api/getUserDetails.php?token=RDN8suCd9Unll6zThEiXvUViJiyrGH3bqa3gE7pQdSti1S7nwk6ekzA4MrGawBmu').success(function(data){
     $scope.LoginKey = data.User.Login;
-    $scope.Credits= data.User.NumberCredits;
+    $scope.Credits = data.User.NumberCredits
   })
 })
 
+.controller("QrCodeScanner", function($scope, $cordovaBarcodeScanner) {
 
-.controller('LoginCtrl', function($scope, $stateParams) {
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-  };
-});
+    $scope.scanBarcode = function() {
+        $cordovaBarcodeScanner.scan().then(function(imageData) {
+            alert(imageData.text);
+            console.log("Barcode Format -> " + imageData.format);
+            console.log("Cancelled -> " + imageData.cancelled);
+        }, function(error) {
+            console.log("An error happened -> " + error);
+        });
+    };
 
-/*.controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state) {
-
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-  };
-
-
-  $scope.data = {};
-
-  $scope.login = function() {
-    LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
-      $state.go('tab.dash');
-    }).error(function(data) {
-      var alertPopup = $ionicPopup.alert({
-        title: 'Login failed!',
-        template: 'Please check your credentials!'
-      });
-    });
-  }
 })
 
-  */
+.controller("DoughnutCtrl", function ($scope) {
+  $scope.labels = ["Wortelen", "Spruiten", "Spinazie"];
+  $scope.data = [300, 500, 100];
+});
